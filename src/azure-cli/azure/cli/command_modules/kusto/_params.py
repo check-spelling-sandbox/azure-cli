01b9378cffadf3e73635fbba7cd88c02c6c5bc6c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-07-06 23:59:27 -0400

    spelling: explanation
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/azure-cli/azure/cli/command_modules/kusto/_params.py
+++ src/azure-cli/azure/cli/command_modules/kusto/_params.py
@@ -18,7 +18,7 @@ def load_arguments(self, _):
     # Kusto clusters
     sku_arg_type = CLIArgumentType(help='The name of the sku.',
                                    arg_type=get_enum_type(AzureSkuName))
-    time_format_explenation = 'Duration in ISO8601 format (for example, 100 days would be P100D).'
+    time_format_explanation = 'Duration in ISO8601 format (for example, 100 days would be P100D).'
 
     with self.argument_context('kusto cluster') as c:
         c.ignore('kusto_management_request_options')
@@ -31,8 +31,8 @@ def load_arguments(self, _):
         c.ignore('kusto_management_request_options')
         c.argument('cluster_name', help='The name of the cluster.', id_part='name')
         c.argument('database_name', arg_type=name_type, help='The name of the database.', id_part='child_name_1')
-        c.argument('soft_delete_period', help='Amount of time that data should be kept so it is available to query. ' + time_format_explenation)
-        c.argument('hot_cache_period', help='Amount of time that data should be kept in cache.' + time_format_explenation)
+        c.argument('soft_delete_period', help='Amount of time that data should be kept so it is available to query. ' + time_format_explanation)
+        c.argument('hot_cache_period', help='Amount of time that data should be kept in cache.' + time_format_explanation)
 
     # Kusto database list
     with self.argument_context('kusto database list') as c:
